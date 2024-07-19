5e1d97fbd0e7a95d15fb85126fb16a1eb2563138
jahnavi1818 <138894623+jahnavi1818@users.noreply.github.com>
2024-07-19 20:22:58 +0530

    Create inputformats.py

---
+++ Day1/inputformats.py
@@ -0,0 +1,21 @@
+name = input("enter your name\n")
+print("hello",name)
+
+age=21
+name="jahnavi"
+print(f" Hello {name} your age {age} years old")
+
+
+name = input()
+s1=int(input())
+s2=int(input())
+s3=int(input())
+print(f"your marks are{s1+s2+s3}, and avg is {(s1+s2+s3)/3}")
+
+name=input()
+print(name,end=" ")
+print(name)
+
+name=input()
+print(name,end="\n")
+print(name)
