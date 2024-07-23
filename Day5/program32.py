0c531772f96f67111ed36ddad76b6964de15f73b
jahnavi1818 <138894623+jahnavi1818@users.noreply.github.com>
2024-07-23 22:14:15 +0530

    Create program32.py

---
+++ Day5/program32.py
@@ -0,0 +1,22 @@
+#Strings
+#String methods are
+#is alam
+#is Lower
+#is Upper
+#is Alpha
+#Check with lower
+#Convert it into Upper case
+#Title Case
+#Swap case
+
+
+str=input()
+print(str.lower())
+print(str.upper())
+print(str.capitalize())
+print(str.title())
+print(str.swapcase())
+print(str.strip()) #related to spaces where starting and ending spaces are deleted
+print(str.replace('a','z'))
+print(str.split())# string is converted to lists 
+print(str.split('a'))
